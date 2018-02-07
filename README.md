d5ce81633442218510b7ed2888a04d7046cd3bd0
Hebh <hebaohua@hust.edu.cn>
2018-02-07 15:46:17 +0800

    添加调用API识别验证码的方式

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # pet-chain-buyer
-百度莱茨狗抢购脚本，采用多线程查询购买，Google tesseract-ocr识别验证码和手动输入验证码两种方式，代码持续优化中
+百度莱茨狗抢购脚本，采用多线程查询购买，支持调用API识别验证友，Google tesseract-ocr识别验证码以及手动输入验证码三种方式，代码持续优化中
 
-参考 [pet-chain](https://github.com/yanwii/pet-chain) 项目
+# 项目介绍
+最近百度上线的莱茨狗区块链宠物[项目地址](https://pet-chain.baidu.com/)支持买入卖出操作，使用本脚本的目的就是找到下图中的低价狗，迅速买入，然后高价卖出，通过低买高卖来积累微积分
+
+![image](https://github.com/Acamy/Images/blob/master/2018-02-07_095853.png)
+
 
 # 使用方法
 - 配置好需要的依赖库
@@ -17,8 +21,17 @@
 
 ![image](https://github.com/Acamy/Images/blob/master/2.png)
 
-### 2. 自动输入验证码
+### 2. 调用ShowApi识别验证码
+[API使用地址](https://www.showapi.com/api/view/184)，该API识别率还是非常高的，花1毛钱即可体验，平均识别时间2-3秒，如下图：
+![image](https://github.com/Acamy/Images/blob/master/2018-02-07_153002.png)
+##### a. 修改config/config.ini里ShowAPI为中的appid和sign为自己的
+##### b. 然后执行`python pet-chain.py`
+![image](https://github.com/Acamy/Images/blob/master/2018-02-07_152735.png)
+
+### 3. 自动输入验证码
 ##### a. 修改config/config.ini的isAuto为True
 
 ##### b. 执行`python pet-chain.py`
 现在自动输入验证码正确率比较低，建议采用第一种方式，后续进行代码优化
+
+参考 [pet-chain](https://github.com/yanwii/pet-chain) 项目
--- pet_chain.py
+++ pet_chain.py
@@ -7,6 +7,7 @@ import configparser
 import traceback
 import base64
 from PIL import Image
+from urllib import request, parse
 import ocr
 
 class PetChain():
@@ -33,13 +34,16 @@ class PetChain():
     def get_config(self):
         config = configparser.ConfigParser()
         config.read("config/config.ini")
-        self.isAuto = config.getboolean("Pet-Chain", "isAuto")
+        self.mode = config.getint("Pet-Chain", "mode")
+        self.isThreadPool = config.getboolean("Pet-Chain", "isThreadPool")
         for i in range(5):
             try:
                 amount = config.getfloat("Pet-Chain", self.degree_map.get(i))
             except Exception as e:
                 amount = 100
             self.degree_conf[i] = amount
+        self.showapi_appid = config.get("ShowAPI", "appid")
+        self.showapi_sign = config.get("ShowAPI", "sign")
 
     def get_headers(self):
         with open("config/headers.txt") as f:
@@ -65,29 +69,42 @@ class PetChain():
             }
             page = requests.post("https://pet-chain.baidu.com/data/market/queryPetsOnSale", headers=self.headers, data=json.dumps(data))
             errorMsg = page.json().get(u"errorMsg")
-            print(errorMsg + '================================================')
+
             if errorMsg == u"success":
                 pets = page.json().get(u"data").get("petsOnSale")
-                pool = threadpool.ThreadPool(10)
-                req = threadpool.makeRequests(self.purchase, pets)
-                for queue in req:
-                    pool.putRequest(queue)
-                pool.wait()
+
+                if self.isThreadPool:
+                    pool = threadpool.ThreadPool(10)
+                    req = threadpool.makeRequests(self.purchase, pets)
+                    for queue in req:
+                        pool.putRequest(queue)
+                    pool.wait()
+                else:
+                    pet0 = pets[0]
+                    self.purchase(pet0)
+
         except Exception as e:
-            print(e)
+            print('Exception in get_market!')
 
     def purchase(self, pet):
         try:
             pet_amount = pet.get(u"amount")
             pet_degree = pet.get(u"rareDegree")
             setting = self.degree_conf.get(pet_degree)
-
+            category = self.degree_map.get(pet_degree)
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ', 当前最低价格: ' + pet_amount + ", 种类为: " + category + ", petId为:" + pet.get(u"petId"))
             if float(pet_amount) <= setting:
-                if self.genCaptcha(): # 生成验证码
-                    img = Image.open("./captcha.jpg")
-                    if self.isAuto:
+                imgstr = self.getCaptchaStr()
+                if imgstr != "error":
+                    print("============================正在尝试购买============================")
+                    if self.mode == 1:
+                        self.genCaptchaImg(imgstr)
+                        img = Image.open("./captcha.jpg")
                         self.captcha = ocr.ocr_img(img)
+                    elif self.mode == 2:
+                        self.captcha = self.getCaptchaByApi(imgstr)
                     else:
+                        self.genCaptchaImg(imgstr)
                         self.captcha = input('请输入验证码：')
 
                     pet_id = pet.get(u"petId")
@@ -101,34 +118,59 @@ class PetChain():
                             "appId": 1,
                             "tpl": ""
                             }
-                    category = self.degree_map.get(pet_degree)
                     page = requests.post("https://pet-chain.baidu.com/data/txn/create", headers=self.headers, data=json.dumps(data), timeout=2)
-                    print(category + ':' + pet_amount + "; " + json.dumps(page.json(), ensure_ascii=False))
+
+                    print(json.dumps(page.json(), ensure_ascii=False))
         except Exception as e:
             traceback.print_exc()
 
-    def genCaptcha(self):
+    def getCaptchaStr(self):
         data = {
             "appId": 1,
             "requestId": int(round(time.time() * 1000)),
             "tpl": "",
         }
         page = requests.post("https://pet-chain.baidu.com/data/captcha/gen", headers=self.headers, data=json.dumps(data), timeout=2)
+
         jPage = page.json()
+
         if jPage.get(u"errorMsg") == "success":
             self.seed = jPage.get(u"data").get(u"seed")
-            img = jPage.get(u"data").get(u"img")
-            with open('captcha.jpg', 'wb') as f:
-                f.write(base64.b64decode(img))
-            return True
+            return jPage.get(u"data").get(u"img")
         else:
             print('获取验证码失败！')
-            return False
+            return "error"
 
+    def genCaptchaImg(self, imgStr):
+        if imgStr != "error":
+            with open('captcha.jpg', 'wb') as f:
+                f.write(base64.b64decode(imgStr))
+
+    def getCaptchaByApi(self, img):
+        self.get_config()
+        url = "http://route.showapi.com/184-5"
+        send_data = parse.urlencode([
+            ('showapi_appid', self.showapi_appid)
+            , ('showapi_sign', self.showapi_sign)
+            , ('img_base64', img)
+            , ('typeId', "34")
+            , ('convert_to_jpg', "0")
+
+        ])
+
+        req = request.Request(url)
+        try:
+            response = request.urlopen(req, data=send_data.encode('utf-8'), timeout=10)  # 10秒超时反馈
+        except Exception as e:
+            print(e)
+        result = response.read().decode('utf-8')
+        result_json = json.loads(result)
+        print('result_json data is:', result_json)
+        return result_json.get(u"showapi_res_body").get(u"Result")
 
 if __name__ == "__main__":
     pc = PetChain()
-    #pc.genCaptcha()
+    #pc.getCaptchaStr()
     while True:
         pc.get_market()
-        time.sleep(3) # 每隔三秒执行一次
+        time.sleep(2) # 每隔三秒执行一次
